2d0ab29218ea76470b8188961aa70600128c9249
BringerOfPlagues <si.wuerfele2@gmail.com>
2025-09-24 15:04:11 +0200

    Schleife wird jetzt abgebrochen, wenn beim Durchlauf der inneren Schleife nichts vertauscht wurde (Array ist ja dann schon sortiert)

--- untitled/src/BubbleSortExample.java
+++ untitled/src/BubbleSortExample.java
@@ -3,6 +3,7 @@ public class BubbleSortExample {
     static void bubbleSort(int[] arr) {
         int n = arr.length;
         int temp = 0;
+        boolean swapped = false;
 
         for (int i = 0; i < n; i++) {
             for (int j = 1; j < (n - i); j++) {
@@ -11,7 +12,11 @@ public class BubbleSortExample {
                     temp = arr[j - 1];
                     arr[j - 1] = arr[j];
                     arr[j] = temp;
+                    swapped = true;
                 }
+            if (!swapped) {
+                break;
+            }
             }
         }
     }
